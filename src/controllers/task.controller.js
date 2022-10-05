cd2ee2129d71814f2ca5388d2606d00988982cb2
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-05 10:04:08 -0400

    manejo de errer sencillo

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,17 +1,18 @@
 const db = require("../db");
 
 //listar tareas
-const getTasks = async (req, res) => {
+const getTasks = async (req, res, next) => {
   try {
     const result = await db.query("select * from task");
     res.json(result.rows);
   } catch (error) {
-    res.json({ error: error.message });
+    // res.json({ error: error.message });
+    next(error);
   }
 };
 
 //listar 1 tarea por ID
-const getTask = async (req, res) => {
+const getTask = async (req, res, next) => {
   //req.params.id -> obtiene el parametro que mandamos por la url
   try {
     const { id } = req.params;
@@ -25,14 +26,13 @@ const getTask = async (req, res) => {
 
     res.json(result.rows[0]);
   } catch (error) {
-    res.json({ error: error.message });
+    // res.json({ error: error.message });
+    next(error);
   }
-
-  res.send("listar una sola tarea");
 };
 
 //Crear una tarea
-const createTask = async (req, res) => {
+const createTask = async (req, res, next) => {
   const { title, description } = req.body;
 
   try {
@@ -42,41 +42,51 @@ const createTask = async (req, res) => {
     );
     res.json(result.rows[0]);
   } catch (error) {
-    res.json({ error: error.message });
+    // res.json({ error: error.message });
+    next(error);
   }
-  res.send("crear una tarea");
 };
 
 //Eliminar una tarea por Id
-const deleteTask = async (req, res) => {
-  const { id } = req.params;
-  const result = await db.query("delete from task where id = $1 returning *", [
-    id,
-  ]);
+const deleteTask = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await db.query(
+      "delete from task where id = $1 returning *",
+      [id]
+    );
 
-  if (result.rowCount === 0)
-    return res.status(404).send({ message: "task not found" });
+    if (result.rowCount === 0)
+      return res.status(404).send({ message: "task not found" });
 
-  //   res.json(result.rows[0]); // retona 200 y muestra el objeto eliminado
-  res.sendStatus(204); // solo retorna 204 cuando elimina
+    //   res.json(result.rows[0]); // retona 200 y muestra el objeto eliminado
+    // solo retorna 204 cuando elimina
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
 };
 
 //Actualizar tarea
-const updateTask = async (req, res) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
+const updateTask = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
 
-  const result = await db.query(
-    "update task set title= $1, description= $2 where id = $3 returning *",
-    [title, description, id]
-  );
+    const result = await db.query(
+      "update task set title= $1, description= $2 where id = $3 returning *",
+      [title, description, id]
+    );
 
-  if (result.rows.length === 0)
-    return res.status(404).json({
-      message: "task not found",
-    });
+    if (result.rows.length === 0)
+      return res.status(404).json({
+        message: "task not found",
+      });
 
-  return res.json(result.rows[0]);
+    return res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -9,5 +9,12 @@ app.use(morgan("dev"));
 //para que express puede entener formato JSON
 app.use(express.json());
 app.use(taskRoutes);
+
+app.use((error, req, res, next) => {
+  return res.json({
+    message: error.message,
+  });
+});
+
 app.listen(4000);
 console.log("Server listo en el puerto 4000");
