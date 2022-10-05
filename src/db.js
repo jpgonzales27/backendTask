f2fc74b888c876e30403fcac35e4c416a58c68e6
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-05 10:44:58 -0400

    usando dotenv para guardar nuestras variables de entorno

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require("pg");
+const { env } = require("./config");
 
 const db = new Pool({
-  user: "postgres",
-  password: "toor",
-  host: "localhost",
-  port: 5432,
-  database: "taskdb",
+  user: env.user,
+  password: env.password,
+  host: env.host,
+  port: env.port,
+  database: env.database,
 });
 
 module.exports = db;
