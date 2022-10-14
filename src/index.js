4897e1bf2240ee72e595989115b08d5dd08923d0
Juan Pablo Gonzales <juampi7237@gmail.com>
2022-10-14 19:25:08 -0400

    add cors para conectar con la UI

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan"); //morgan nos ayuda a ver los tipos de peticionces q se realizan
+const cors = require("cors"); //realiza la comunicacion entre los servers
 
 const taskRoutes = require("./routes/task.routes");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 //para que express puede entener formato JSON
 app.use(express.json());
